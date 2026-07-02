600a94097ddb9f3f7b3c15e6f196d51cbd085adb
Jivonz Dy <jibonsayness@gmail.com>
2026-07-03 04:35:28 +0800

    Removed 1 unused import

--- backend/src/main/java/com/inventory/system/controller/NotificationController.java
+++ backend/src/main/java/com/inventory/system/controller/NotificationController.java
@@ -2,7 +2,6 @@ package com.inventory.system.controller;
 
 import com.inventory.system.entity.Product;
 import com.inventory.system.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
